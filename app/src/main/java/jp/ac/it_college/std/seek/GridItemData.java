6d6a10953664ab16747f547bf08a533797f25cd2
s13012 <s13012@std.it-college.ac.jp>
2015-04-30 17:01:25 +0900

    画像のリソースIDのデータ構造を配列からリストに変更

--- app/src/main/java/jp/ac/it_college/std/seek/GridItemData.java
+++ app/src/main/java/jp/ac/it_college/std/seek/GridItemData.java
@@ -1,16 +1,19 @@
 package jp.ac.it_college.std.seek;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class GridItemData {
-    private int[] itemResNumbers = {
+    private List<Integer> itemResNumbers = Arrays.asList(
             R.drawable.akuma, R.drawable.bat, R.drawable.farao_white
             ,R.drawable.goburin, R.drawable.harpy, R.drawable.sraim
             ,R.drawable.ahriman, R.drawable.alraune, R.drawable.devildoll_bk
             ,R.drawable.earth_worm, R.drawable.katana, R.drawable.kinoko
             ,R.drawable.knight, R.drawable.moai, R.drawable.mummy
             ,R.drawable.seed, R.drawable.tarantula, R.drawable.yadokari
-    };
+    );
 
-    public int[] getItemResNumbers() {
+    public List<Integer> getItemResNumbers() {
         return itemResNumbers;
     }
 
